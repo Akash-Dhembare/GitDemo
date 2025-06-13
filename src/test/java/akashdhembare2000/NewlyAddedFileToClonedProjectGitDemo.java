0eba7bafbc495bea66dc2ddd75d84c0092e9ae65
Akash <akash.dhembare777@gmail.com>
2025-06-13 13:12:11 +0530

    This is commit from First Guy

---
+++ src/test/java/akashdhembare2000/NewlyAddedFileToClonedProjectGitDemo.java
@@ -0,0 +1,7 @@
+package akashdhembare2000;
+
+public class NewlyAddedFileToClonedProjectGitDemo {
+    public static void main(String[] args) {
+        System.out.println("This is Newly Added File To Cloned Project GitDemo");
+    }
+}
