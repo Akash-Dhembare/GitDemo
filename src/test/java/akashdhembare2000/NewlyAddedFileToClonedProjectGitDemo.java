a9bf29500505c39a31ba747ff9151ac8329b92e9
Akash <akash.dhembare777@gmail.com>
2025-06-13 17:31:49 +0530

    “Added”

--- src/test/java/akashdhembare2000/NewlyAddedFileToClonedProjectGitDemo.java
+++ src/test/java/akashdhembare2000/NewlyAddedFileToClonedProjectGitDemo.java
@@ -1,7 +1,16 @@
 package akashdhembare2000;
 
+import org.testng.annotations.Test;
+
 public class NewlyAddedFileToClonedProjectGitDemo {
     public static void main(String[] args) {
         System.out.println("This is Newly Added File To Cloned Project GitDemo");
     }
+
+    @Test
+    public void NewTest(){
+        System.out.println("Use of branches");
+    }
 }
+
+
